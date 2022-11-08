2b440d43860c37131de78ddfaf9c659c386dcb7f
Khushboo Jalan <khushj3@gmail.com>
2022-11-08 15:28:51 +0800

    Adding old sh file

--- backend/legalEntity.php
+++ backend/legalEntity.php
@@ -1,55 +1,71 @@
 <?php
-    /**
-     * Adyen Checkout Example (https://www.adyen.com/)
-     * Copyright (c) 2019 Adyen BV (https://www.adyen.com/)
-     * /paymentMethods Documentation: https://docs.adyen.com/api-explorer/#/PaymentSetupAndVerificationService/v40/paymentMethods
-     */
+/**
+ * Adyen Checkout Example (https://www.adyen.com/)
+ * Copyright (c) 2019 Adyen BV (https://www.adyen.com/)
+ * /paymentMethods Documentation: https://docs.adyen.com/api-explorer/#/PaymentSetupAndVerificationService/v40/paymentMethods
+ */
 
-    /**
-     * Use this as a template to make API calls using php
-     *
-     */
+/**
+ * Use this as a template to make API calls using php
+ *
+ */
 
-     //Get data from frontend
-    if (file_get_contents('php://input') != '') {
-        $request = json_decode(file_get_contents('php://input'), true);
-    } else {
-        $request = array();
-    }
+ //Get data from frontend
+if (file_get_contents('php://input') != '') {
+    $request = json_decode(file_get_contents('php://input'), true);
+} else {
+    $request = array();
+}
 
-    $username = $_ENV["LEM_BASICAUTH_USERNAME"];
-    $password = $_ENV["LEM_BASICAUTH_PWD"];
-    $url = "https://kyc-test.adyen.com/lem/v1/legalEntities"; //call endpoint here
+$username = $_ENV["LEM_BASICAUTH_USERNAME"];
+$password = $_ENV["LEM_BASICAUTH_PWD"];
+$url = "https://kyc-test.adyen.com/lem/v1/legalEntities"; //call endpoint here
 
-    //Add any additional data not sent in the request
-    $data = [];
+//Add any additional data not sent in the request
+$data = [];
 
-    // Convert data to JSON
-    $json_data = json_encode(array_merge($data, $request));
+// Convert data to JSON
+$json_data = json_encode(array_merge($data, $request));
 
-    // Set the url
-    curl_setopt($curlAPICall, CURLOPT_URL, $url);
+// Initiate curl
+$curlAPICall = curl_init();
 
-    //Basic Auth
+// Set to POST
+curl_setopt($curlAPICall, CURLOPT_CUSTOMREQUEST, "POST");
 
-    curl_setopt($curlAPICall, CURLOPT_HEADER, false);
-    curl_setopt($curlAPICall, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($curlAPICall, CURLOPT_USERPWD, $username.":".$password);
+// Will return the response, if false it print the response
+curl_setopt($curlAPICall, CURLOPT_RETURNTRANSFER, true);
 
-    // Api key
-    curl_setopt($curlAPICall, CURLOPT_HTTPHEADER,
-        array(
-            "Content-Type: application/json",
-            "Content-Length: " . strlen($json_data)
-        )
+// Add JSON message
+curl_setopt($curlAPICall, CURLOPT_POSTFIELDS, $json_data);
 
-    // Error Check
-    if ($result === false){
-        throw new Exception(curl_error($curlAPICall), curl_errno($curlAPICall));
-    }
+// Set the url
+curl_setopt($curlAPICall, CURLOPT_URL, $url);
 
-    // Closing
-    curl_close($curlAPICall);
+//Basic Auth
 
-    // This file returns a JSON object
-   print($result);
+curl_setopt($curlAPICall, CURLOPT_HEADER, false);
+curl_setopt($curlAPICall, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($curlAPICall, CURLOPT_USERPWD, $username.":".$password);
+
+// Api key
+curl_setopt($curlAPICall, CURLOPT_HTTPHEADER,
+    array(
+        "Content-Type: application/json",
+        "Content-Length: " . strlen($json_data)
+    )
+);
+
+// Execute
+$result = curl_exec($curlAPICall);
+
+// Error Check
+if ($result === false){
+    throw new Exception(curl_error($curlAPICall), curl_errno($curlAPICall));
+}
+
+// Closing
+curl_close($curlAPICall);
+
+// This file returns a JSON object
+print($result);
---
+++ start_kj.sh
@@ -0,0 +1,11 @@
+export MERCHANT_ACCOUNT="KhushBP_Partner"
+export CHECKOUT_APIKEY="AQEwhmfxLY7GaBVBw0m/n3Q5qf3VY4NYHptoaEZZy2iqkH/0lmhRxApJmnzhHCp0ls01EMFdWw2+5HzctViMSCJMYAc=-bFv50p5OuDFd75BbYMJuWBP7G63qh1cUwiqWGscgvck=-g9{7fa4K(Z:HfDSe"
+export CLIENT_KEY="test_4BEILGUC4JBDLEYJ7TGNLJXBUIN4676K"
+export API_VERSION="v69"
+
+export LEM_BASICAUTH_USERNAME="ws_481111@Scope.Company_KhushBPCompany"
+export LEM_BASICAUTH_PWD="^,4#TxpFcMcr~Px,fYkJ[.kT("
+
+export PLATFORM_APIKEY="AQE7hmfuXduSOksR5lq8k3A+qPaWBr9IHodoWWlXyHuurmpKisF5AstKDiJyBl4sjdmRXLQkHrIEmf0fIYcQwV1bDb7kfNy1WIxIIkxgBw==-zkCdi/44TJwghypVDswrlvQn60SRL7d/z5WoDnaQ61Q=-&H:Sh>~MSy%74\$Rj"
+
+php -S localhost:3000
