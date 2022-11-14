dd14ed3e8a7f358f8ea34960d23bd3fa4451f3c0
jasone <88962615+ohhpls@users.noreply.github.com>
2022-11-14 13:52:32 +0800

    Update UI

--- frontend/dashboardWidgets.js
+++ frontend/dashboardWidgets.js
@@ -6,7 +6,7 @@ async function callDashboardWidgets()
 
   //console.log(response['balances'][0]['available']);
   var getBalance = document.getElementById("getBalance");
-  getBalance.innerText = "SGD "+getBalanceResponse['balances'][0]['available'];
+  getBalance.innerText = "SGD "+(getBalanceResponse['balances'][0]['available']/100.00).toFixed(2);
   getBalance.hidden = false;
 
   // var getBalanceReserved = document.getElementById("getBalanceReserved");
